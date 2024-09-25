34548be44e9b01f0a9b16c245aee79c7ef34c09c
zhaoce <fifpoet@qq.com>
2024-09-25 13:54:58 +0800

    kfrsnd

--- server/handler/resq/music.go
+++ server/handler/resq/music.go
@@ -6,12 +6,26 @@ import (
 )
 
 type UpsertMusicReq struct {
-	MusicId int                   `json:"music_id"`
-	Cover   string                `json:"cover"`
-	File    *multipart.FileHeader `json:"file"`
+	RootId      int                   `json:"music_id"`
+	Title       string                `json:"title"`
+	ArtistIds   []int                 `json:"artist_ids"`
+	Composer    int                   `json:"composer"`
+	Writer      int                   `json:"writer"`
+	IssueYear   int                   `json:"issue_year"`
+	Language    string                `json:"language"`
+	PerformType string                `json:"perform_type"`
+	Instrument  string                `json:"instrument"`
+	Concert     string                `json:"concert"`
+	ConcertYear int                   `json:"concert_year"`
+	Sequence    int                   `json:"sequence"`
+	MvUrl       string                `json:"mv_url"`
+	CoverOss    *multipart.FileHeader `json:"cover_oss"`
+	MpOss       *multipart.FileHeader `json:"mp_oss"`
+	LyricOss    *multipart.FileHeader `json:"lyric_oss"`
+	SheetOss    *multipart.FileHeader `json:"sheet_oss"`
 }
 
-func (u *UpsertMusicReq) ToModel() (*mysql.Music, *multipart.FileHeader) {
+func (u *UpsertMusicReq) ToModel() (m *mysql.Music, cover, mp, lyric, sheet *multipart.FileHeader) {
 
-	return nil, nil
+	return nil, nil, nil, nil, nil
 }
