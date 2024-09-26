01cf3c1a1692147e3869aff6ccdd57f876c02a65
zhaoce <fifpoet@qq.com>
2024-09-27 00:34:20 +0800

    kfrsnd

--- server/infra/mysql/0_init.go
+++ server/infra/mysql/0_init.go
@@ -16,12 +16,12 @@ func NewMysqlCli() *gorm.DB {
 	dsn := "root:%s@tcp(%s:3306)/footprint?charset=utf8mb4&parseTime=True&loc=Local"
 	var err error
 	Db, err := gorm.Open(mysql.New(mysql.Config{
-		DSN:                       fmt.Sprint(fmt.Sprintf(dsn, mysqlPwd, ip)), // DSN data source name
-		DefaultStringSize:         256,                                        // string 类型字段的默认长度
-		DisableDatetimePrecision:  true,                                       // 禁用 datetime 精度，MySQL 5.6 之前的数据库不支持
-		DontSupportRenameIndex:    true,                                       // 重命名索引时采用删除并新建的方式，MySQL 5.7 之前的数据库和 MariaDB 不支持重命名索引
-		DontSupportRenameColumn:   true,                                       // 用 `change` 重命名列，MySQL 8 之前的数据库和 MariaDB 不支持重命名列
-		SkipInitializeWithVersion: false,                                      // 根据当前 MySQL 版本自动配置
+		DSN:                       fmt.Sprintf(dsn, mysqlPwd, ip), // DSN data source name
+		DefaultStringSize:         256,                            // string 类型字段的默认长度
+		DisableDatetimePrecision:  true,                           // 禁用 datetime 精度，MySQL 5.6 之前的数据库不支持
+		DontSupportRenameIndex:    true,                           // 重命名索引时采用删除并新建的方式，MySQL 5.7 之前的数据库和 MariaDB 不支持重命名索引
+		DontSupportRenameColumn:   true,                           // 用 `change` 重命名列，MySQL 8 之前的数据库和 MariaDB 不支持重命名列
+		SkipInitializeWithVersion: false,                          // 根据当前 MySQL 版本自动配置
 	}), &gorm.Config{
 		NamingStrategy: schema.NamingStrategy{
 			TablePrefix:   "fp_", // 表名前缀，`User`表为`dp_user`
