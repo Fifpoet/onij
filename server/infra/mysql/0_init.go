3b886e48c00ce37d7716757978a3840ef60c9ca1
zhaoce <fifpoet@qq.com>
2025-04-30 21:25:57 +0800

    aa

--- server/infra/mysql/0_init.go
+++ server/infra/mysql/0_init.go
@@ -15,7 +15,7 @@ import (
 func NewMysqlCli() *gorm.DB {
 	ip := os.Getenv("ip")
 	mysqlPwd := os.Getenv("mypwd")
-	dsn := "myuser:%s@tcp(%s:3306)/footprint?charset=utf8mb4&charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:%s@tcp(%s:3306)/footprint?charset=utf8mb4&charset=utf8mb4&parseTime=True&loc=Local"
 	var err error
 	Db, err := gorm.Open(mysql.New(mysql.Config{
 		DSN:                       fmt.Sprintf(dsn, mysqlPwd, ip), // DSN data source name
