a0b30ebc1ca08dcf4654a38ef9d86329019b6e8d
赵策 <fifpoet@qq.com>
2025-06-20 14:57:25 +0800

    a

--- src/components/layout/PageHeader.vue
+++ src/components/layout/PageHeader.vue
@@ -1,14 +1,10 @@
 <!-- 主导航栏组件 -->
 <template>
-  <MobileHeader v-if="isMobile" />
-  <DesktopHeader v-else />
+  <MobileHeader class="lg:hidden" />
+  <DesktopHeader class="hidden lg:block" />
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-import { useMediaQuery } from '@vueuse/core'
 import MobileHeader from './MobileHeader.vue'
 import DesktopHeader from './DesktopHeader.vue'
-
-const isMobile = useMediaQuery('(max-width: 1024px)')
 </script>
