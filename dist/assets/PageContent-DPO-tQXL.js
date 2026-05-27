84a95830ef74fc5ddfc63bb9ff38aaa575569754
zhaoce <fifpoet@qq.com>
2026-05-27 15:42:43 +0800

    a

--- dist/assets/PageContent-DPO-tQXL.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,h as n,k as o,W as s}from"./index-Bb0cW83i.js";const c={},r={class:"page-content"};function a(e,_){return o(),n("div",r,[s(e.$slots,"default")])}const d=t(c,[["render",a]]);export{d as P};
---
+++ dist/assets/search-sB6ut1Gz.js
@@ -0,0 +1,1 @@
+import{q as a}from"./index-B5etXI7p.js";async function n(t){const e=t.join(",");return await a("/song/detail",{ids:e})}export{n as g};
