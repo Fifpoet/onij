8f584ad73ab7d776b3e4d69e5ae95f5f9fa49190
zhaoce <fifpoet@qq.com>
2026-05-27 15:32:48 +0800

    a

--- release/web/assets/search-EWguECU6.js
+++
@@ -1,1 +0,0 @@
-import{q as a}from"./index-DNAwY7D8.js";async function n(t){const e=t.join(",");return await a("/song/detail",{ids:e})}export{n as g};
---
+++ release/web/assets/search-Wfq6oS9-.js
@@ -0,0 +1,1 @@
+import{q as a}from"./index-Bb0cW83i.js";async function n(t){const e=t.join(",");return await a("/song/detail",{ids:e})}export{n as g};
